6fb535ce3798d9625ebe76837311641e3ee2017b
Ryan Ho <apexial@aol.com>
2023-04-03 01:55:24 -0400

    final commit

--- app.py
+++ app.py
@@ -1,76 +1,105 @@
-
-from flask import Flask, render_template, request, render_template_string, redirect, send_file
+from flask import (
+    Flask,
+    render_template,
+    request,
+    render_template_string,
+    redirect,
+    send_file,
+)
 from pyopenms import MSExperiment, MzXMLFile
 import io, os, string, re
 from properties import *
-import pyopenms as oms
+import requests
 
 app = Flask(__name__)
 
 
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-#Main Flask App
-@app.route("/", methods = ["GET"])
+# Main Flask App
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-@app.route("/info", methods = ["GET", "POST"])
+
+@app.route("/info", methods=["GET", "POST"])
 def info():
-     # Check if a file is present in the request
-    if 'fileUpload' not in request.files:
-        #flash('No file found')
+    # Check if a file is present in the request
+    if "fileUpload" not in request.files:
         return redirect(request.url)
 
-    file = request.files['fileUpload']
+    file = request.files["fileUpload"]
 
     # If the file is empty or has an invalid extension, show an error message
-    if file.filename == '' or not allowed_file(file.filename):
-        #flash('Invalid file')
+    if file.filename == "" or not allowed_file(file.filename):
         return redirect(request.url)
 
     # Save the file to the upload folder and parse it
     if file and allowed_file(file.filename):
         filename = file.filename
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
         file.save(file_path)
 
-        #flash('File uploaded and parsed successfully') 
         mzxml_file = filename
         header = create_header(mzxml_file)
         body = list(create_body(mzxml_file))
         meta = parse_ms_instrument(mzxml_file)
         scanCount = extract_scan_count(mzxml_file)
 
-        return render_template("table.html", header=header, body = body, meta = meta, scanCount = scanCount, mzxml_file = mzxml_file)
+        return render_template(
+            "table.html",
+            header=header,
+            body=body,
+            meta=meta,
+            scanCount=scanCount,
+            mzxml_file=mzxml_file,
+        )
 
 
-@app.route("/peaks/<mzxml_file>/<int:value>", methods = ["GET", "POST"])
+@app.route("/peaks/<mzxml_file>/<int:value>", methods=["GET", "POST"])
 def peaks(mzxml_file, value):
-    if (request.method == "GET"):
+    if request.method == "GET":
         peaks = detect_peaks(mzxml_file, value)
         peakLen = len(peaks)
         iterator = 0
-        possibleCombo = []
         index = "scan=" + str(value)
-        while (iterator < len(peaks)):
-            combo = findCombo(peaks[iterator][index][0])
-            possibleCombo.append(combo)
-            iterator += 1
 
-        print(possibleCombo)
-
-        return render_template("peaks.html", value = value, index = index, peaks = peaks, peakLen = peakLen, possibleCombo = possibleCombo)
+        return render_template(
+            "peaks.html", value=value, index=index, peaks=peaks, peakLen=peakLen
+        )
     else:
-        return render_template("peaks.html", peaks = peaks)
+        return render_template("peaks.html", peaks=peaks)
 
-@app.route("/convert/<mzxml_file>", methods = ["POST", "GET"])
+
+@app.route("/convert/<mzxml_file>", methods=["POST", "GET"])
 def convert(mzxml_file):
     xml_file = "output.xml"
     mzxml_to_xml(mzxml_file, xml_file)
 
     return send_file(xml_file, as_attachment=True)
 
-if __name__ == '__main__':
+
+@app.route("/compound/<value>/<t>/<mz>/<intensity>", methods=["POST", "GET"])
+def compound(value, mz, t, intensity):
+    if request.method == "GET":
+        compoundInfo = findCompound(mz)
+        compoundName = defCompound(compoundInfo)
+        peakNum = t
+        breakdown = composition(compoundInfo)
+        return render_template(
+            "compound.html",
+            breakdown=breakdown,
+            intensity=intensity,
+            value=value,
+            mz=mz,
+            compoundInfo=compoundInfo,
+            compoundName=compoundName,
+            peakNum=peakNum,
+        )
+    else:
+        return render_template("compound.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- properties.py
+++ properties.py
@@ -1,13 +1,16 @@
-import re
+import re, requests
 import pyopenms as oms
 from pyopenms import MSExperiment, MzXMLFile
 import xml.etree.ElementTree as ET
-import xml.dom.minidom
+from molmass import Formula
 
 # Checks for file extension - extra protocol
-ALLOWED_EXTENSIONS = {'mzxml'}
+ALLOWED_EXTENSIONS = {"mzxml"}
+
+
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 # Table Header Function
 def create_header(mzxml_file):
@@ -29,9 +32,10 @@ def create_header(mzxml_file):
                 # Extract the headers (keys) from the properties
                 headers = []
                 for prop in properties_list:
-                    key, _ = prop.split('=', 1)
+                    key, _ = prop.split("=", 1)
                     headers.append(key)
     return headers
+
 
 # Extract and Create Table Body Function
 def create_body(mzxml_file):
@@ -54,15 +58,15 @@ def create_body(mzxml_file):
                 # Parse the properties into a dictionary
                 properties_dict = {}
                 for prop in properties_list:
-                    key, value = prop.split('=', 1)
+                    key, value = prop.split("=", 1)
                     properties_dict[key] = value.strip('"')
-                    
+
                 # Append properties_dict to the scan_list
                 scan_list.append(properties_dict)
     return scan_list
 
-# Metadata
 
+# Metadata
 def parse_attribute(tag_string):
     attribs = {}
     tag_parts = re.findall(r'(\S+)=("[^"]*"|\'[^\']*\')', tag_string)
@@ -71,45 +75,50 @@ def parse_attribute(tag_string):
         attribs[name] = value
     return attribs
 
+
 def parse_ms_instrument(xml_file):
-    with open(xml_file, 'r') as file:
+    with open(xml_file, "r") as file:
         content = file.read()
 
-    start_ms_instrument = content.find('<msInstrument>') + len('<msInstrument>')
-    end_ms_instrument = content.find('</msInstrument>')
+    start_ms_instrument = content.find("<msInstrument>") + len("<msInstrument>")
+    end_ms_instrument = content.find("</msInstrument>")
     ms_instrument_content = content[start_ms_instrument:end_ms_instrument]
 
     lst = []
     ms_instrument = {}
     while True:
-        start_tag = ms_instrument_content.find('<') + 1
-        end_tag = ms_instrument_content.find('>')
+        start_tag = ms_instrument_content.find("<") + 1
+        end_tag = ms_instrument_content.find(">")
         if start_tag == 0 or end_tag == -1:
             break
 
         open_tag = ms_instrument_content[start_tag:end_tag]
-        if ' ' not in open_tag:
+        if " " not in open_tag:
             # Empty tag, skip
-            ms_instrument_content = ms_instrument_content[end_tag + 1:]
+            ms_instrument_content = ms_instrument_content[end_tag + 1 :]
             continue
 
         close_tag = open_tag.split()[0]
-        start_close_tag = ms_instrument_content.find('</' + close_tag + '>') + len('</' + close_tag + '>')
+        start_close_tag = ms_instrument_content.find("</" + close_tag + ">") + len(
+            "</" + close_tag + ">"
+        )
         ms_instrument_content = ms_instrument_content[start_close_tag:]
 
         attribs = parse_attribute(open_tag)
         ms_instrument[close_tag] = attribs
     return ms_instrument
 
-#Scan Count
+
+# Scan Count
 def extract_scan_count(mzxml_file):
-    with open(mzxml_file, 'r') as file:
+    with open(mzxml_file, "r") as file:
         for line in file:
-            if 'scanCount' in line:
-                start = line.index('scanCount=') + 11
+            if "scanCount" in line:
+                start = line.index("scanCount=") + 11
                 end = line.index('"', start)
                 return int(line[start:end])
     return None
+
 
 # Get peaks
 def detect_peaks(file_path, scan_num):
@@ -127,41 +136,67 @@ def detect_peaks(file_path, scan_num):
 
     # Print the detected peaks
     for spectrum in picked_exp:
-      current_scan_num = spectrum.getNativeID()
-      if current_scan_num.lstrip("scan=") == str(scan_num):
-         for peak in spectrum:
-            mz = peak.getMZ()
-            intensity = peak.getIntensity()
-            compound = findCombo(mz)
-            lst.append({current_scan_num : [mz, intensity]})
+        current_scan_num = spectrum.getNativeID()
+        if current_scan_num.lstrip("scan=") == str(scan_num):
+            for peak in spectrum:
+                mz = peak.getMZ()
+                intensity = peak.getIntensity()
+                lst.append({current_scan_num: [mz, intensity]})
     return lst
- 
-def findCombo(molarMass):
-    carbonMass = 12.01
-    hydrogenMass = 1.008
-    oxygenMass = 16.00
-    tolerance = 0.1
 
-    c = 1
-    h = 0
-    o = 0
 
-    while c <= int(molarMass/carbonMass):
-        while h <= int(molarMass/hydrogenMass):
-            while o <= int(molarMass/oxygenMass):
-                currentMolarMass = c * carbonMass + h * hydrogenMass + o * oxygenMass;
-                
-                if abs(currentMolarMass - molarMass) < tolerance:
-                    return [c, h, o]
-            
-                o += 1
-            h += 1       
-            o = 0
-        c += 1 
-        h = 0
+def searchCompound(mz):
+    url = "https://pubchem.cheminfo.org/mfs/em?em=" + str(mz)
+
+    wow = requests.get(url)
+    result = wow.content
+    result = result.decode("UTF-8")
+
+    string_to_search = str(result)
+    start_index = string_to_search.index('"_id":"') + len('"_id":"')
+    end_index = start_index
+    for i in range(start_index, len(string_to_search)):
+        if string_to_search[i] == '"':
+            end_index = i
+            break
+    desired_string = string_to_search[start_index:end_index]
+    return desired_string
+
+
+def findCompound(mz):
+    try:
+        return searchCompound(mz)
+    except ValueError:
+        return "Compound not found"
+
+
+def defCompound(formula):
+    url = "https://pubchem.cheminfo.org/molecules/mf?mf=" + str(formula)
+    wow = requests.get(url)
+    temp = wow.content
+    temp = temp.decode("UTF-8")
+    start_index = temp.find("iupac")
+    # Find the index of the closing double quotes after "iupac"
+    end_index = temp.find('"', start_index + 8)
+    # Extract the substring between start_index and end_index
+    result = temp[start_index + 8 : end_index]
+
+    if result == "ull," or result == "t":
+        return "No formal name/Compound not found"
+    else:
+        return result
+
+
+def composition(formula):
+    if (formula == "Compound not found"):
+        return "Unknown/Compound not found"
+    f = Formula(formula)
+    return f.composition()
+
 
 def remove_namespace(tag):
-    return tag.split('}')[-1]
+    return tag.split("}")[-1]
+
 
 def mzxml_to_xml(mzxml_file, xml_file):
     # Parse the mzXML file
@@ -187,5 +222,3 @@ def mzxml_to_xml(mzxml_file, xml_file):
     # Write the new XML file
     new_tree = ET.ElementTree(new_root)
     new_tree.write(xml_file, encoding="utf-8", xml_declaration=True)
-    # dom = xml.dom.minidom.parse(xml_file) # or xml.dom.minidom.parseString(xml_string)
-    # pretty_xml_as_string = dom.toprettyxml()
